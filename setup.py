320540d49e7b2f8a47db0830405408ad1da09c8c
James Kelly <jkelly@sensoryinc.com>
2022-09-25 01:18:04 -0700

    0.1.23

--- setup.py
+++ setup.py
@@ -1,12 +1,29 @@
+from distutils.command.sdist import sdist as sdist_orig
+from distutils.errors import DistutilsExecError
 from setuptools import setup, find_packages
 import pathlib
+
+
+# an act of desperation and arguably least worst case to get a great tool
+class sdist(sdist_orig):
+    def run(self):
+        try:
+            self.spawn(['pip', 'install', 'https://s3-us-west-2.amazonaws.com/ai2-s2-scispacy/releases/v0.4.0/en_core_sci_md-0.4.0.tar.gz'])
+        except DistutilsExecError:
+            self.warn('listing directory failed')
+        super().run()
+
+
 
 here = pathlib.Path(__file__).parent.resolve()
 long_description = (here / 'README.md').read_text(encoding='utf-8')
 
+
+
+
 setup(
     name='clinproc',  
-    version='0.1.22',
+    version='0.1.23',
     description='library for processing clinical trials data from clinicaltrials.gov',
     long_description=long_description,
     long_description_content_type='text/markdown',  
@@ -30,17 +47,15 @@ setup(
             'lxml',
             'scispacy',
             'negspacy',
-            'en_core_sci_md'
-    ],
-    dependency_links=[
-        "https://s3-us-west-2.amazonaws.com/ai2-s2-scispacy/releases/v0.4.0/en_core_sci_md-0.4.0.tar.gz#egg=en_core_sci_md"
     ],
     entry_points={  
         'console_scripts': [
             'clinproc=clinproc:main',
         ],
     },
-
+    cmdclass={
+        'sdist': sdist
+    },
     project_urls={  
         'Bug Reports': 'https://github.com/semajyllek/clinproc/issues',
         'Source': 'https://github.com/semajyllek/clinproc/',
