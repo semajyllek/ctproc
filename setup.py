3edd0e1051aeb515f7ff41e8659b5b46e9014a82
James Kelly <jkelly@sensoryinc.com>
2022-09-25 01:45:43 -0700

    0.1.25

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
     name='clinproc',  
-    version='0.1.24',
+    version='0.1.25',
     description='library for processing clinical trials data from clinicaltrials.gov',
     long_description=long_description,
     long_description_content_type='text/markdown',  
@@ -47,7 +47,7 @@ setup(
             'lxml',
             'scispacy',
             'negspacy',
-            'en_core_sci_md @ https://s3-us-west-2.amazonaws.com/ai2-s2-scispacy/releases/v0.4.0/en_core_sci_md-0.4.0.tar.gz'
+            'en_core_sci_md@https://s3-us-west-2.amazonaws.com/ai2-s2-scispacy/releases/v0.4.0/en_core_sci_md-0.4.0.tar.gz#egg=en_core_sci_md-0.4.0'
     ],
     entry_points={  
         'console_scripts': [
