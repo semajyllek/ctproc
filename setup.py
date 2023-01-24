bc0264089be4289de6894760a259dfe346f5fa71
James Kelly <jkelly@sensoryinc.com>
2023-01-24 13:12:18 +0100

    version bump

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ctproc",
-    version="0.0.1",
+    version="0.1.0",
     author="James Kelly",
     author_email="mrkellyjam@gmail.com",
     description="library for processing clinical trials data from clinicaltrials.gov",
